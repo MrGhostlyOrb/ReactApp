ebf545af572490a993b3fb9e8821b29d32befeae
Ben Cooper <bassbencooper999@gmail.com>
2019-08-30 17:19:48 +0100

    Finished Counting application
    Counting Application depricated and moved onto new application in main index file.

--- src/index-V2.js
+++ src/index-V2.js
@@ -2,17 +2,68 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
+const History = (props) => {
 	
-	const [value, setValue] = useState(10)
+	if (props.allClicks.length === 0){
+		
+		return(
+		
+			<div>
+				The app is used by pressing the buttons.
+			</div>
+		
+		)
+		
+	}
 	
 	return(
 	
 		<div>
-			{value}
-			<button onClick = {() => setValue(0)}>reset to zero</button>
+			Button press history: {props.allClicks.join(' ')}
 		</div>
 	
+	)
+	
+}
+
+const Button = ({ onClick, text }) => (
+
+	<button onClick = {onClick}>
+		{text}
+	</button>
+
+)
+
+const App = (props) => {
+	
+	const [left, setLeft] = useState(0)
+	const [right, setRight] = useState(0)
+	const [allClicks, setAll] = useState([])
+	
+	const handleLeftClick = () => {
+		
+		setAll(allClicks.concat('L'))
+		setLeft(left + 1)
+		
+	}
+	
+	const handleRightClick = () => {
+		
+		setAll(allClicks.concat('R'))
+		setRight(right + 1)
+		
+	}
+	
+	return(
+		<div>
+			<div>
+				{left}
+				<Button onClick = {handleLeftClick} text = 'Left'/>
+				<Button onClick = {handleRightClick} text = 'Right'/>
+				{right}
+				<History allClicks = {allClicks}/>
+			</div>
+		</div>
 	)
 	
 }
